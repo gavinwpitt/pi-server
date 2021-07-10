a1270c0680a9e4712a731e6ded0f580593d5c45c
gavinwpitt <gavinwpitt@gmail.com>
2021-07-10 12:30:26 -0400

    pushing old commits

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,13 @@ import express, { Express } from "express";
 import cors from "cors";
 import weatherRoutes from "./routes/weather";
 import rfremoteRoutes from "./routes/rfremote";
+import spotifyRoutes from "./routes/spotify";
+import alarmRoutes from "./routes/alarm";
 import WeatherCollector from "./models/WeatherCollector";
 import RfRemote from "./models/RfRemote";
+import Alarm from "./models/Alarm";
 import MySqlWrapper from "./lib/MySqlWrapper";
+import SpotifyApiWrapper from "./lib/SpotifyApiWrapper";
 
 dotenv.config();
 
@@ -17,6 +21,8 @@ const PORT: string | number = parseInt(process.env.PORT || "") ?? 4000;
 console.log(process.env.PORT);
 console.log(process.env.SQL_USER)
 console.log(process.env.BME280_i2cAddress);
+console.log(process.env.SPOTIFY_CLIENT_ID);
+console.log(process.env.SPOTIFY_SECRET_KEY);
 
 // Create and set up MySql instance
 let mySqlInstance:MySqlWrapper = new MySqlWrapper();
@@ -32,12 +38,21 @@ let weatherCollector:WeatherCollector = new WeatherCollector(mySqlInstance, rfre
 weatherCollector.setup();
 app.set('weatherCollector', weatherCollector);
 
+// Set up Spotify
+let spotifyApiWrapper = new SpotifyApiWrapper(process.env.SPOTIFY_CLIENT_ID || "", process.env.SPOTIFY_SECRET_KEY || "");
+app.set('spotifyApiWrapper', spotifyApiWrapper);
+
+Alarm.SetMySqlInstance(mySqlInstance);
+Alarm.SetSpotifyWrapper(spotifyApiWrapper);
+
 app.use(express.json());
 app.use(express.urlencoded());
 
 app.use(cors())
 app.use(weatherRoutes)
 app.use(rfremoteRoutes)
+app.use(spotifyRoutes);
+app.use(alarmRoutes);
 
 app.listen( PORT, () => {
     console.log("server started at port " + PORT);
